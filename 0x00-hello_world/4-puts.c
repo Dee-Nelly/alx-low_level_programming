4038ad9618f067589329e41894960048ad50146c
Nelly <dimakatsomabasa.dm@gmail.com>
2023-07-13 18:02:16 -0700

    My first c program at ALX

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - uses puts to print output
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
