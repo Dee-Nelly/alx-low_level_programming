5afd642d25b2df9cc3625c18ef0e53afccb7419e
Nelly <dimakatsomabasa.dm@gmail.com>
2023-07-17 17:38:33 -0700

    Added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints only unique combinantions
+ * of digits 0-9 strictly using putchar function
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar('0' + num1);
+			putchar('0' + num2);
+
+			if (num1 == 8 && num2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
