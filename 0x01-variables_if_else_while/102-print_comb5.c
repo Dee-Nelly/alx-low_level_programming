48afbbe0417f99ff457884238c19c69ed9358502
Nelly <dimakatsomabasa.dm@gmail.com>
2023-07-17 18:29:11 -0700

    Added 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinantions
+ *	of two pairs of two digit numbers, strictly
+ *	using the putchar function.
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 99; num2++)
+		{
+			putchar('0' + num1);
+			putchar('0' + num1);
+			putchar(' ');
+			putchar('0' + num2);
+			putchar('0' + num2);
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
