43351dc7c9d471cbdb82556f5864341b0c738446
Nelly <dimakatsomabasa.dm@gmail.com>
2023-10-27 19:28:57 -0700

    Fixed code for task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,13 +16,9 @@ char *_strcat(char *dest, char *src)
 	while (dest[i] != '\0')
 		i++;
 
-	j = 0;
-	while (src[j] != '\0')
-	{
-		j++;
-		i++;
+	for (j = 0; src[j] != '\0'; j++, i++)
 		dest[i] = src[j];
-	}
+
 	dest[i] = '\0';
 	return (dest);
 }
