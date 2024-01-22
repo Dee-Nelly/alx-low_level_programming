77015cf06c52ff2f94ac0d96c4b17d7aa4f87dec
Nelly Mabasa <dimakatsomabasa.dm@gmail.com>
2024-01-22 08:43:22 -0800

    Added task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - finds a character in a string
+ * @s: string to be searched
+ * @c: character being looked for
+ *
+ * Return: pointer to first occurrence of @c in @s
+ *	and NULL if character @c absent
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef FILE_NAME
 #define FILE_NAME
 
+#include <stddef.h>
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-
+char *_strchr(char *s, char c);
 #endif
