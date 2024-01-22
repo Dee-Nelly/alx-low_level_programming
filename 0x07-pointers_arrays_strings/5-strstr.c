1f566d5814ddb80c204685ebfc08beb1c6db1a3d
Nelly Mabasa <dimakatsomabasa.dm@gmail.com>
2024-01-22 13:33:44 -0800

    Addded task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - prints consecutive characters of sring 1 that are in string 2
+ * @haystack: source string (string 1)
+ * @needle: searching string (string 2)
+ *
+ * Return: pointer to beginnig of substring in @haystack
+ *	otherwise NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	if (*needle == '\0')
+		return (haystack);
+	while (*haystack)
+	{
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (haystack[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+
+		}
+		haystack++;
+	}
+	return (0);
+}
