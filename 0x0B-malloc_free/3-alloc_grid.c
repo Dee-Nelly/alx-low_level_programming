809f59c68926c66fe08281f6669279a0ba375d8c
Nelly Mabasa <dimakatsomabasa.dm@gmail.com>
2024-04-11 12:43:29 -0700

    Allocated the right amount of space and freed the memory correctly

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,12 +22,12 @@ int **alloc_grid(int width, int height)
 	if (grid == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			for (i--; i <= 0; i--)
+			for (i--; i >= 0; i--)
 				free(grid[i]);
 			free(grid);
 			return (NULL);
