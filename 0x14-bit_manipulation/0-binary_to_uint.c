262c5c2fbc374915e4de88c2aa385134e3a3aab2
Nelly <dimakatsomabasa.dm@gmail.com>
2024-06-26 03:30:39 -0700

    Added 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a number from binary to a decimal
+ * @b: a  pointer to a string of 0's and 1's to be converted
+ *
+ * Return: 0 if the string is not comprised of strictly
+ *	0's and 1's or if b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		decimal = decmimal << 1;
+
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			decimall = decimal | 1;
+		b++;
+	}
+
+	return (decimal);
+}
